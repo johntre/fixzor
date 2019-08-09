5842e06dcba91891ca0cb4b5a78bdd0975c95727
John Drake <johndrake@Johns-MBP-2.Home>
2019-08-08 20:21:42 -0400

    adding delete button

--- controllers/landing.js
+++ controllers/landing.js
@@ -45,7 +45,7 @@ exports.show_edit_lead = function(req, res, next) {
 exports.edit_lead = function(req, res, next) {
   req.params.lead_id
   req.body.lead_email
-  
+
 
   return models.Lead.update({
     email: req.body.lead_email
@@ -55,5 +55,15 @@ exports.edit_lead = function(req, res, next) {
     }
   }).then(result => {
     res.redirect('/lead/' + req.params.lead_id);
+  })
+}
+
+exports.delete_lead = function(req, res, next) {
+  return models.Lead.destroy({
+    where: {
+      id: req.params.lead_id
+    }
+  }).then(result => {
+      res.redirect('/Leads');
   })
 }
